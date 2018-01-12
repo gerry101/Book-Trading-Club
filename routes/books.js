1d76dc2cb239430f3a29174997bb9b126e99a80b
unknown <Gerry@GMigwi17-LT.AfricanLeadershipAcademy.local>
2018-01-12 16:05:23 +0200

    Fix env. value

--- routes/books.js
+++ routes/books.js
@@ -25,7 +25,7 @@ var cloudinary = require('cloudinary');
 cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME, 
   api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET: 
+  api_secret: process.env.API_SECRET
 });
 
 // All books show route
